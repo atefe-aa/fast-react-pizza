352ee411d8b8d8bbc4ece72c6d1674aa3d277cd2
atefe-aa <aa.asgariyan12@gmail.com>
2023-09-01 11:17:00 +0330

    add search order feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -21,7 +21,6 @@ const router = createBrowserRouter([
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
-
       },
       {
         path: "/cart",
@@ -34,6 +33,9 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+
       },
     ],
   },
